afea15da77cb397ce272e7aad7412c66f18fdd5c
Syed Affan Hussain <syedaffan.dev@gmail.com>
2024-01-11 00:46:11 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+<div align="center"><h2>💻 DevFolio - Portfolio for Developers</h2></div>
+<h4>Designed live portfolio using HTML, CSS and JavaScript.</h4>
+<img align="center" alt="GIF" src="Demo.png" />
+
+<h2>🔗 Useful links</h2>
+<h4>🌐 Web App: https://syedaffan10.github.io/ </h4>
---
+++ js/app.js
@@ -0,0 +1,92 @@
+//Header fixed
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+}
+
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active")
+    }
+    this.classList.add("active");
+    document.querySelector(".navbar").classList.toggle("show");
+  })
+})
+
+//Hamburger
+
+const hamBurger = document.querySelector(".hamburger");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("show");
+})
+
+//Portfolio Gallery
+
+const filterBtn = document.querySelector("#filterBtn").children;
+const item = document.querySelector(".gallery").children;
+
+for (let i = 0; i < filterBtn.length; i++) {
+  filterBtn[i].addEventListener("click", function () {
+    for (let j = 0; j < filterBtn.length; j++) {
+      filterBtn[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    const target = this.getAttribute("data-target");
+    for (let k = 0; k < item.length; k++) {
+      item[k].style.display = "none";
+      if (target == item[k].getAttribute("data-id")) {
+        item[k].style.display = "block";
+      }
+      if (target == "all") {
+        item[k].style.display = "block";
+      }
+    }
+  });
+}
+
+const closeLightbox = document.querySelector(".close-lightbox");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = lightbox.querySelector("img");
+
+lightbox.addEventListener("click", function () {
+  if (event.target != lightboxImg) {
+    lightbox.classList.remove("show");
+    lightbox.classList.add("hide");
+  }
+});
+
+closeLightbox.addEventListener("click", function () {
+  lightbox.classList.remove("show");
+  lightbox.classList.add("hide");
+});
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = document.querySelectorAll(".item");
+
+galleryItem.forEach(function (element) {
+  element.querySelector(".fa-plus").addEventListener("click", function () {
+    lightbox.classList.remove("hide");
+    lightbox.classList.add("show");
+    lightboxImg.src = element.querySelector("img").getAttribute("src");
+  });
+});
+
+//Footer Year 
+var year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
